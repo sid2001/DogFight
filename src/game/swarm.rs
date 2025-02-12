55417d1741b4862f2c6a1bf3fbeb32748f966031
siddharth <sidharth2001.lumia@gmail.com>
2025-02-12 17:52:09 +0530

    not great not bad

--- src/game/swarm.rs
+++ src/game/swarm.rs
@@ -3,6 +3,8 @@ use bevy::{math::VectorSpace, prelude::*, state::commands};
 use rand::Rng;
 use std::borrow::{Borrow, BorrowMut};
 use std::cell::RefCell;
+use std::collections::HashMap;
+use std::ops::Deref;
 use std::time::Duration;
 
 use super::spaceship::Health;
@@ -27,7 +29,35 @@ pub struct SwarmPoint {
     radius: f32,
 }
 
-#[derive(Component)]
+#[derive(Clone)]
+pub struct SwarmData {
+    leader: Option<Entity>,
+    followers: u32,
+    followers_limit: u32,
+    state: SwarmBotState,
+}
+
+#[derive(Resource, Clone)]
+pub struct SwarmTracker(HashMap<Entity, SwarmData>);
+
+impl Default for SwarmData {
+    fn default() -> Self {
+        Self {
+            leader: None,
+            followers: 0,
+            followers_limit: 5,
+            state: SwarmBotState::Solo,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum SwarmBotState {
+    Swarming,
+    InSwarm,
+    Solo,
+}
+#[derive(Component, Clone)]
 #[require(SceneRoot)]
 pub struct SwarmBot {
     dir: Dir3,
@@ -40,6 +70,9 @@ pub struct SwarmBot {
     health: f32,
     in_swarm: bool,
     target_distance: f32,
+    swarm_data: SwarmData,
+    state: SwarmBotState,
+    swarm_up_distance: f32,
 }
 
 impl Default for SwarmBot {
@@ -50,11 +83,14 @@ impl Default for SwarmBot {
             target_dir: Dir3::Y,
             thrust: 0.,
             thrust_limit: 10.,
-            angular_velocity: 80.,
+            angular_velocity: 200.,
             drag: Vec3::ZERO,
             velocity: Vec3::ZERO,
             in_swarm: false,
             target_distance: 9999.,
+            swarm_data: SwarmData::default(),
+            state: SwarmBotState::Solo,
+            swarm_up_distance: 10.,
         }
     }
 }
@@ -63,10 +99,10 @@ impl Default for SwarmPoint {
     fn default() -> Self {
         Self {
             origin: Vec3::ZERO,
-            burst_count: 20,
-            cooldown: Duration::new(10, 0),
+            burst_count: 5,
+            cooldown: Duration::new(2, 0),
             last_burst: Duration::new(0, 0),
-            limit: 40,
+            limit: 20,
             live_bots: 0,
             radius: 2.,
         }
@@ -82,11 +118,20 @@ impl SwarmPoint {
 pub struct SwarmPlugin;
 impl Plugin for SwarmPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
+        app.insert_resource(SwarmTracker(HashMap::new()))
+            .add_systems(Startup, setup)
             .add_systems(Update, release_bots)
             .add_systems(
                 Update,
-                (detect_target, avoidance, steer, accelerate, move_bots).chain(),
+                (
+                    detect_target,
+                    swarm_up,
+                    avoidance,
+                    steer,
+                    accelerate,
+                    move_bots,
+                )
+                    .chain(),
             );
     }
 }
@@ -115,6 +160,7 @@ fn release_bots(
     mut query_swarm_point: Query<(&Transform, &mut SwarmPoint), With<SwarmPointMarker>>,
     scene_assets: Res<SceneAssets>,
     time: Res<Time>,
+    mut swarm_tracker: ResMut<SwarmTracker>,
 ) {
     let time_delta = time.delta();
     for (trans, mut swarm_point) in query_swarm_point.iter_mut() {
@@ -143,7 +189,7 @@ fn release_bots(
             let bot = SwarmBot {
                 dir: Dir3::new(dir).unwrap_or(Dir3::Y),
                 velocity: dir * 0.0,
-                thrust: 2.,
+                thrust: 0.,
                 drag: Vec3::ZERO,
                 ..default()
             };
@@ -154,7 +200,8 @@ fn release_bots(
                 .with_scale(Vec3::new(0.2, 0.2, 0.2))
                 .looking_to(bot.dir.clone(), Dir3::Y);
             let scene = SceneRoot(scene_assets.bot_spaceship.clone());
-            commands.spawn((bot, transform, scene, SwarmBotMarker));
+            let entity = commands.spawn((bot, transform, scene, SwarmBotMarker)).id();
+            swarm_tracker.0.insert(entity, SwarmData::default());
         }
     }
 }
@@ -240,7 +287,7 @@ fn detect_target(
             if dist > target_dist {
                 target = trans_target.translation;
                 dist = target_dist;
-                info!("target detected");
+                // info!("target detected");
             }
         });
 
@@ -266,7 +313,10 @@ fn detect_target(
     // }
 }
 
-fn avoidance(mut query_bots: Query<(Entity, &Transform, &mut SwarmBot), With<SwarmBotMarker>>) {
+fn avoidance(
+    mut query_bots: Query<(Entity, &Transform, &mut SwarmBot), With<SwarmBotMarker>>,
+    swarm_tracker: Res<SwarmTracker>,
+) {
     let bots: Vec<_> = query_bots
         .iter()
         .map(|(e, t, _)| (e.clone(), t.clone()))
@@ -275,23 +325,134 @@ fn avoidance(mut query_bots: Query<(Entity, &Transform, &mut SwarmBot), With<Swa
     for (e1, t1, mut b1) in query_bots.iter_mut() {
         let mut dist: f32 = 999.;
         let mut obj = Vec3::ZERO;
-        for (e2, t2) in bots.iter() {
-            if e1 == *e2 {
-                continue;
+        if let Some(swarm_data) = swarm_tracker.0.get(&e1) {
+            match swarm_data.state {
+                SwarmBotState::Solo => {
+                    for (e2, t2) in bots.iter() {
+                        if e1 == *e2 {
+                            continue;
+                        }
+                        let diff = (t2.translation - t1.translation).length();
+                        if dist > diff {
+                            dist = diff;
+                            obj = t2.translation;
+                        }
+                    }
+                    if dist < 0.2 {
+                        let avoid_dir = (obj - t1.translation).normalize_or_zero();
+                        let mut dir = avoid_dir.cross(t1.forward().as_vec3());
+                        if dir == Vec3::ZERO {
+                            dir = avoid_dir.cross(t1.left().as_vec3());
+                        }
+                        b1.target_dir = Dir3::new(dir).unwrap_or(Dir3::Y);
+                    }
+                }
+                _ => (),
             }
-            let diff = (t2.translation - t1.translation).length();
-            if dist > diff {
-                dist = diff;
-                obj = t2.translation;
-            }
-        }
-        if dist < 0.2 {
-            let avoid_dir = (obj - t1.translation).normalize_or_zero();
-            let mut dir = avoid_dir.cross(t1.forward().as_vec3());
-            if dir == Vec3::ZERO {
-                dir = avoid_dir.cross(t1.left().as_vec3());
-            }
-            b1.target_dir = Dir3::new(dir).unwrap_or(Dir3::Y);
+        } else {
+            error!("Entity should be present in hashmap!!");
         }
     }
 }
+
+fn swarm_up(
+    mut query_bots: Query<(Entity, &Transform, &mut SwarmBot), With<SwarmBotMarker>>,
+    mut swarm_tracker: ResMut<SwarmTracker>,
+) {
+    let bots: Vec<_> = query_bots
+        .iter()
+        .map(|(e, t, b)| (e.clone(), t.clone(), b.clone()))
+        .collect();
+
+    for (e1, t1, mut b1) in query_bots.iter_mut() {
+        // let mut dist = 999.;
+        // let mut target_bot = Vec3::ZERO;
+        // let swarm_map = swarm_tracker.0;
+        if let Some(swarm_data) = swarm_tracker.0.clone().get(&e1) {
+            match swarm_data.state {
+                SwarmBotState::Solo => {
+                    for (e2, t2, b2) in bots.iter() {
+                        if e1 == *e2 {
+                            continue;
+                        }
+                        if (t1.translation - t2.translation).length() <= b1.swarm_up_distance {
+                            //swarm up only when coming from behind
+                            //right swarming up the first bot it see, not with the closest one
+                            let costh = b1.velocity.normalize().dot(b2.velocity.normalize());
+                            if costh < 0. {
+                                continue;
+                            } else {
+                                //first swarm up with the first one later move to its leader if it has space like a balanced tree
+                                // b1.swarm_data.leader = Some(e2.clone());
+                                // let t_swarm_data: SwarmData;
+                                swarm_tracker.0.entry(e2.clone()).and_modify(|d| {
+                                    d.state = SwarmBotState::InSwarm;
+                                    d.followers += 1;
+                                    d.leader = Some(e2.clone());
+                                });
+                                info!("{} swarming with {}", e1.to_bits(), e2.to_bits());
+                                swarm_tracker.0.entry(e1.clone()).and_modify(|d| {
+                                    d.state = SwarmBotState::Swarming;
+                                    d.leader = Some(e2.clone());
+                                });
+                                b1.target_dir = Dir3::new(
+                                    (t2.translation - t1.translation).normalize_or(Vec3::Y),
+                                )
+                                .unwrap();
+                                break;
+                                // b1.state = SwarmBotState::Swarming;
+                                // swarm_data.state = SwarmBotState::Swarming;
+                                // swarm_data.leader = Some(e2.clone());
+                                //think something to update the followers list
+                                // b2.swarm_data.followers += 1;
+                            }
+                        }
+                    }
+                }
+                SwarmBotState::Swarming => {
+                    let leader = swarm_tracker.0.get(&e1).unwrap().leader.unwrap();
+                    let leader_transform = bots
+                        .iter()
+                        .find(|(e, _, _)| *e == leader)
+                        .map(|(_, t, b2)| (t.clone(), b2.clone()));
+                    if let Some((l_trans, b2)) = leader_transform {
+                        // Perform operations with leader_transform
+                        if (l_trans.translation - t1.translation).length() <= 0.5 {
+                            swarm_tracker.0.entry(e1.clone()).and_modify(|d| {
+                                d.state = SwarmBotState::InSwarm;
+                            });
+                            b1.target_dir = l_trans.forward();
+                            info!("{} in swarm with", e1.to_bits());
+                            b1.thrust = b2.thrust;
+                        } else {
+                            b1.target_dir = Dir3::new(
+                                (l_trans.translation - t1.translation).normalize_or(Vec3::ZERO),
+                            )
+                            .unwrap();
+                            info!("{} swarming with", e1.to_bits());
+                            b1.thrust = b2.thrust + 0.1;
+                        }
+                    } else {
+                        swarm_tracker.0.entry(e1.clone()).and_modify(|d| {
+                            d.state = SwarmBotState::Solo;
+                        });
+                    }
+                }
+                _ => (),
+            }
+        } else {
+            error!("Entity should be present!!");
+        }
+    }
+}
+
+// fn swarming(mut query_bots: Query<(Entity, &Transform), With<SwarmBotMarker>>) {
+//     for
+// }
+
+//todo
+// get a way to prevent the leader from following it's follower
+// while in swarm manitain distance and maintain alignment with neigbours
+// try to serialize swarm, communicate swarm members of their global leaders
+// make the thrust control more stable and dynamic
+// give name to swarms so that avoidance mechanics still works if outer swarm bot tries to cut
